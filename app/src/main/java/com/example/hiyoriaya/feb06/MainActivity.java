2bb2dacbb6b423469e54e7f09632df6d986cdd5a
hiyoriaya <hiyoriaya@gmail.com>
2016-02-07 23:43:17 +0900

    Viewをかさねたりしたい

--- app/src/main/java/com/example/hiyoriaya/feb06/MainActivity.java
+++ app/src/main/java/com/example/hiyoriaya/feb06/MainActivity.java
@@ -2,17 +2,18 @@ package com.example.hiyoriaya.feb06;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements View.OnTouchListener{
+public class MainActivity extends Activity implements CompoundButton.OnCheckedChangeListener{
 
     RelativeLayout rl;
     LinearLayout ll;
+    int select;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,22 +28,20 @@ public class MainActivity extends Activity implements View.OnTouchListener{
 
         for(int i=0;i<3;i++){
             RadioButton txt = new RadioButton(this);
-            txt.setText(i+"番目");
+            txt.setOnCheckedChangeListener(this);
+            txt.setText(i + "番目");
             txt.setTextSize(20);
             ll.addView(txt);
-            ll.getChildAt(i).setOnTouchListener(this);
         }
     }
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
-
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         TextView txt = new TextView(this);
         txt.setText("its a beautiful world");
         txt.setTextSize(20);
         rl.addView(txt);
-        rl.getChildAt(0).setY(v.getBottom());
-
-        return true;
+        rl.getChildAt(0).setY(buttonView.getBottom());
+        buttonView.setVisibility(View.INVISIBLE);
     }
 }
